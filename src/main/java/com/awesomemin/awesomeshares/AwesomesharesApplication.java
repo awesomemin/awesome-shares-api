d31f1b7c7a447984df7154d45c1c224bb10dd3bc
Seongmin Oh <saogig0503@naver.com>
2026-01-22 18:39:42 +0900

    initial commit

---
+++ src/main/java/com/awesomemin/awesomeshares/AwesomesharesApplication.java
@@ -0,0 +1,13 @@
+package com.awesomemin.awesomeshares;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwesomesharesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwesomesharesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/awesomemin/awesomeshares/AwesomesharesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.awesomemin.awesomeshares;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwesomesharesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
