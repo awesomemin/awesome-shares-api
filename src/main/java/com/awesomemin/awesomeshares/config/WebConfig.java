94cb5a330e64864a08c0c052a14c40627015ab25
Seongmin Oh <saogig0503@naver.com>
2026-01-24 15:08:30 +0900

    feat: CORS 정책 설정

---
+++ src/main/java/com/awesomemin/awesomeshares/config/WebConfig.java
@@ -0,0 +1,22 @@
+package com.awesomemin.awesomeshares.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    public static final String ALLOWED_METHOD_NAMES = "GET,HEAD,POST,PUT,DELETE,TRACE,OPTIONS,PATCH";
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods(ALLOWED_METHOD_NAMES.split(","))
+                .allowedHeaders("*")
+                .exposedHeaders(HttpHeaders.CONTENT_DISPOSITION)
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
